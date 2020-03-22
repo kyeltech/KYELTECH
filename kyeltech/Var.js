95955de8579892d4842a175c1a36be9d42794234
kyeltech <kyel2030@gmail.com>
2020-03-22 03:11:55 +0100

    update

---
+++ kyeltech/Var.js
@@ -0,0 +1,6 @@
+var name;
+var name = " Ezekiel Benjamin";
+var courses = ['html', 'css', 'javascript', 'nodejs', 'php']
+ for( name = 20; name < courses.length; name++) {
+     name  = courses[name]
+ }
