1c7027299017ed8ee01bf949c3954726726d89d0
kyeltech <kyel2030@gmail.com>
2020-03-30 00:59:03 +0100

    node task1

--- kyeltech/node/Node.js
+++ kyeltech/node/Node.js
@@ -1,16 +1,35 @@
-var http = require('http');
-var fs = require('fs');
+const http = require('http');
+const fs = require('fs')
+const {parse} = require('querystring')
 
-function onRequest(request, response) {
-  response.writeHead(200);
-  fs.writeFile('./form.html', null, function(error, data) {
-    if (error) {
-      response.writeHead(404);
-      response.write('fiole not found');
-    } else {
-      response.write(data);
-    }
-    response.end();
-  });
-}
-http.createServer(onRequest).listen(8080);
+const server = http.createServer((req, res) =>{
+  let html = `<html>
+    <body>
+      <p>Enter a message here</p>
+      <form action='./message.txt' method='post' name='form1'>
+        <input type='text' value='' name='message' />
+        <input type='submit' />
+      </form>
+    </body>
+  </html> `
+
+  if(req.method === 'POST'){
+    let body =''
+    req.on('data', chunk => {
+      body +=chunk.toString()
+    })
+    req.on('end', () => {
+      let path='.'+ req.url
+      res.writeHead(200, {'content-Type' : 'text/plain'})
+      fs.writeFile(path,parse(body).message.txt.toString(), (err) => {
+        if(err){
+          throw err
+        }
+      })
+      res.end('okay')
+    })
+  } else {
+    res.end(html)
+  }
+})
+server.listen(3000)
