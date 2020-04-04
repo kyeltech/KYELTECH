dc0d7e470d59da0777e5e6006f83d676a1f2182f
kyeltech <kyel2030@gmail.com>
2020-04-04 11:25:34 +0100

    commit

---
+++ kyeltech/first_webpage/ExamplesJS/MyFunction.js
@@ -0,0 +1,62 @@
+// function MyInfo (name) {
+// //  passing a parameters
+// console.log(`my name is ${name}`)
+
+// console.log(` 22,female`)
+
+// return `Yay it works`
+// // returning a statement
+// }
+// let MyName = (`jeff`)
+// // declaring 
+
+//  let x = MyInfo('MyName')
+//  console.log(x)
+// //  how to return a statement 
+
+// Example number two function to test if you are underage
+// function UnderAge(age) {
+//     // put it from a parameter
+//     if ( age < 18 ) {
+//         return true
+//         // block code, thats why we have to pass in the let x = ()
+//     }
+//     // usng an if statement
+//     else {
+//         return false
+//     }
+//     // using an else statement
+// }
+// let x = UnderAge(5);
+// console.log(x)
+
+// function NewAge(age){
+//     return ( age < 18)
+// // without using the ifelse statement we could just pass in the return as you can see cause it will do the same work
+
+// }
+// let x = NewAge(20)
+// console.log(x)
+
+// Example 3 for else if
+// function MyAge(New) {
+//     if( New < 20) {
+//         return true
+//     }
+//     else {
+//         return false
+//     }
+// }
+// let me = MyAge(3)
+// console.log(me)
+
+function name(age, number) {
+    if( age < 12, number > 12) {
+        return true
+    } else {
+        return false
+    }
+
+}
+let x = name(12);
+console.log(x)
